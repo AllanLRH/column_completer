e1fabcd2c2a771c09873d0caf550d04b2016dfcb
Allan L. R. Hansen <allan.lrh@gmail.com>
2019-05-30 15:56:17 +0200

    Autoformatting

--- column_completer.py
+++ column_completer.py
@@ -1,5 +1,6 @@
 class ColumnCompleter(object):
     """Complete Pandas DataFrame column names"""
+
     def __init__(self, df, space_filler='_', silence_warnings=False):
         super(ColumnCompleter, self).__init__()
         self.columns = df.columns
@@ -9,29 +10,29 @@ class ColumnCompleter(object):
             self._warn_about_column_names_edge_spaces()
         self._set_columns()
 
-
     def _warn_about_column_names_edge_spaces(self):
         if not hasattr(self.columns, 'str'):
             return None
         if self.columns.str.startswith(' ').any():
             raise Warning("The following columns starts with one or more spaces: " +
-                           self.columns[self.columns.str.startswith(' ')])
+                          self.columns[self.columns.str.startswith(' ')])
         if self.columns.str.endswith(' ').any():
             raise Warning("The following columns ends with one or more spaces: " +
-                           self.columns[self.columns.str.endswith(' ')])
+                          self.columns[self.columns.str.endswith(' ')])
 
     def _set_columns(self):
         if self.space_filler is None:
             self.mapping = {col: col for col in self.columns if ' ' not in col}
         else:
-            self.mapping = {col.replace(' ', self.space_filler):col for col in self.columns}
+            self.mapping = {col.replace(
+                ' ', self.space_filler): col for col in self.columns}
             if len(self.mapping) < len(self.columns):
                 raise ValueError("Using {} as a replacemnt for".format(repr(self.space_filler)) +
-                    " spaces causes a collision of column names, please chose another.")
+                                 " spaces causes a collision of column names, please chose another.")
         self.keys = self.mapping.keys()
         if len(self.keys) < len(self.columns) and not self.silence_warnings:
             raise Warning("Without a space_filler specified, you're only able to autocomplete " +
-                "{} of {} column names.".format(len(self.keys), len(self.columns)))
+                          "{} of {} column names.".format(len(self.keys), len(self.columns)))
 
     @staticmethod
     def replace_df_column_spaces(df, rep, capatilize_first_letter=False):
@@ -40,7 +41,8 @@ class ColumnCompleter(object):
             raise ValueError("Renaming the columns in such a way would cause a " +
                 "collision of column names.")
         if capatilize_first_letter:
-            rename_dict = {k: v[0].upper() + v[1:] for k, v in rename_dict.items()}
+            rename_dict = {k: v[0].upper() + v[1:]
+                           for k, v in rename_dict.items()}
         return df.rename(columns=rename_dict)
 
     def __dir__(self):
